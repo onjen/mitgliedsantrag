4f89d44ab06cd4af2308624164c6f883cc793ec2
Lars Eschweiler <vreezy117@googlemail.com>
2024-10-02 20:13:04 +0200

    added ContributeTable

--- src/App.tsx
+++ src/App.tsx
@@ -3,205 +3,199 @@ import React from "react";
 import "./App.css";
 
 import {
+  Button,
   FormControl,
   FormControlLabel,
-  FormLabel,
   Radio,
   RadioGroup,
   TextField,
 } from "@mui/material";
-import RadioWrapper from "./RadioWrapper";
+
+import ContributeTable from "./ContributeTable";
 
 function App() {
-  const [selectedValue, setSelectedValue] = React.useState<string>("nothing");
-  const handleRadioChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    checked: boolean
-  ) => {
-    if (checked) {
-      setSelectedValue(event.target.value);
-    }
-  };
+
+
   return (
     <>
-      <div>
-        <h1>Antrag auf Mitgliedschaft </h1>
-        <div>
-          <p>
-            Hier mit beantrage ich die Mitgliedschaft im Erfindergeist Jülich
-            e.V. zum nächstmöglichen Zeitpunkt. Ich habe die Vereinssatzung
-            gelesen und bin mit der Einhaltung derer einverstanden. Mir ist
-            bewusst, dass mit der Annahme des Antrages eine Einladung zu den
-            Kommunikation- & Co-Working-Apps erfolgen und die Bildrechte
-            abgegeben werden.{" "}
-          </p>
-        </div>
-        {/* <h2>Mitgliedsbeitragsmodelle (Bitte ankreuzen):</h2> */}
-        <table>
-          <tr>
-            <th>
-              <p> Mitgliedsbeitragsmodell</p>
-            </th>
-            <th>
-              <p>Monatlich</p>
-            </th>
-            <th>
-              <p> Jährlich</p>
-            </th>
-            <th>
-              <p>Auswahl</p>
-            </th>
-          </tr>
-          <tr>
-            <td>
-              <p>- ordentliches Mitglied</p>
-            </td>
-            <td>
-              <p>10€</p>
-            </td>
-            <td>
-              <p>120€</p>
-            </td>
-            <td>
-              <RadioWrapper
-                checkValue="a"
-                selectedValue={selectedValue}
-                onChange={handleRadioChange}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <p>- Förder Mitgliedschaft:1. Natürliche Personen</p>
-            </td>
-            <td>
-              <p>10€</p>
-            </td>
-            <td>
-              <p>120€</p>
-            </td>
-            <td>
-              <RadioWrapper
-                checkValue="b"
-                selectedValue={selectedValue}
-                onChange={handleRadioChange}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <p>- Förder Mitgliedschaft: 2. Juristische Personen</p>
-            </td>
-            <td>
-              <p>15€</p>
-            </td>
-            <td>
-              <p>180€</p>
-            </td>
-            <td>
-              <RadioWrapper
-                checkValue="c"
-                selectedValue={selectedValue}
-                onChange={handleRadioChange}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td colSpan={3}>
-              <p>
-                - Individuelle Förderung:
-                <br />
-                Ich fördere den Erfindergeist Jülich e.V. mit folgendem Betrag
-                (in Euro, pro Monat):
-                <TextField
-                  id="standard-basic"
-                  label="Monats Beitrag"
-                  variant="standard"
-                  fullWidth
-                />
-                Dieser Betrag wird gesammelt, einmal jährlich überwiesen oder
-                abgebucht. Juristische Personen geben bitte den Namen einer
-                natürlichen Person als Vertreter an.
-              </p>
-            </td>
-            <td>
-              <RadioWrapper
-                checkValue="d"
-                selectedValue={selectedValue}
-                onChange={handleRadioChange}
-              />
-            </td>
-          </tr>
-        </table>
+      <h1>Antrag auf Mitgliedschaft </h1>
 
-        <ul>
-          {selectedValue === "d" && (
-            <li>
+      <div>
+        <p>
+          Hier mit beantrage ich die Mitgliedschaft im Erfindergeist Jülich e.V.
+          zum nächstmöglichen Zeitpunkt. Ich habe die Vereinssatzung gelesen und
+          bin mit der Einhaltung derer einverstanden. Mir ist bewusst, dass mit
+          der Annahme des Antrages eine Einladung zu den Kommunikation- &
+          Co-Working-Apps erfolgen und die Bildrechte abgegeben werden.
+        </p>
+      </div>
+
+      <ContributeTable />
+
+      {/* <table>
+        <tr>
+          <th>
+            <p>Mitgliedsbeitragsmodell</p>
+          </th>
+          <th>
+            <p>Monatlich</p>
+          </th>
+          <th>
+            <p> Jährlich</p>
+          </th>
+          <th>
+            <p>Auswahl</p>
+          </th>
+        </tr>
+        <tr>
+          <td>
+            <p>- ordentliches Mitglied</p>
+          </td>
+          <td>
+            <p>10€</p>
+          </td>
+          <td>
+            <p>120€</p>
+          </td>
+          <td>
+            <RadioWrapper
+              checkValue="a"
+              selectedValue={selectedValue}
+              onChange={handleRadioChange}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>- Förder Mitgliedschaft:1. Natürliche Personen</p>
+          </td>
+          <td>
+            <p>10€</p>
+          </td>
+          <td>
+            <p>120€</p>
+          </td>
+          <td>
+            <RadioWrapper
+              checkValue="b"
+              selectedValue={selectedValue}
+              onChange={handleRadioChange}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>- Förder Mitgliedschaft: 2. Juristische Personen</p>
+          </td>
+          <td>
+            <p>15€</p>
+          </td>
+          <td>
+            <p>180€</p>
+          </td>
+          <td>
+            <RadioWrapper
+              checkValue="c"
+              selectedValue={selectedValue}
+              onChange={handleRadioChange}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td colSpan={3}>
+            <p>
+              - Individuelle Förderung:
+              <br />
+              Ich fördere den Erfindergeist Jülich e.V. mit folgendem Betrag (in
+              Euro, pro Monat):
               <TextField
                 id="standard-basic"
-                label="Unternehmensname"
+                label="Monats Beitrag"
                 variant="standard"
                 fullWidth
-                required={selectedValue === "d"}
               />
-            </li>
-          )}
-          <li>
-            <TextField
-              id="standard-basic"
-              label="Name, Titel"
-              variant="standard"
-              fullWidth
-              required
+              Dieser Betrag wird gesammelt, einmal jährlich überwiesen oder
+              abgebucht. Juristische Personen geben bitte den Namen einer
+              natürlichen Person als Vertreter an (Unternehmensname).
+            </p>
+          </td>
+          <td>
+            <RadioWrapper
+              checkValue="d"
+              selectedValue={selectedValue}
+              onChange={handleRadioChange}
             />
-          </li>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="Vorname"
-              variant="standard"
-              fullWidth
-              required
-            />
-          </li>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="Straße, Hausnummer"
-              variant="standard"
-              fullWidth
-              required
-            />
-          </li>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="PLZ, Ort"
-              variant="standard"
-              fullWidth
-              required
-            />
-          </li>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="Telefon"
-              variant="standard"
-              fullWidth
-              required
-            />
-          </li>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="E-Mail"
-              variant="standard"
-              fullWidth
-              required
-            />
-          </li>
-        </ul>
+          </td>
+        </tr>
+      </table> */}
 
+      <ul>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="Unternehmensname"
+            variant="standard"
+            fullWidth
+          />
+        </li>
+
+        <li>
+          <TextField
+            id="standard-basic"
+            label="Name, Titel"
+            variant="standard"
+            fullWidth
+            required
+          />
+        </li>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="Vorname"
+            variant="standard"
+            fullWidth
+            required
+          />
+        </li>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="Straße, Hausnummer"
+            variant="standard"
+            fullWidth
+            required
+          />
+        </li>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="PLZ, Ort"
+            variant="standard"
+            fullWidth
+            required
+          />
+        </li>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="Telefon"
+            variant="standard"
+            fullWidth
+            required
+          />
+        </li>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="E-Mail"
+            variant="standard"
+            fullWidth
+            required
+          />
+        </li>
+      </ul>
+
+      <div>
         <p>
           Personen unter 16 Jahren sollten sich mit einem Erziehungsberechtigten
           anmelden, siehe Vereinssatzung.
@@ -217,7 +211,6 @@ function App() {
           SDUEDE33XXX Verwendungszweck: Mitgliedsnummer (Wird Ihnen per E-Mail
           mitgeteilt)
         </p>
-
         <TextField
           id="standard-basic"
           label="Unterschrift"
@@ -225,101 +218,103 @@ function App() {
           fullWidth
           disabled
         />
-        <h1>Freiwillige angaben </h1>
-        <ul>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="Beruf/Tätigkeitsort"
-              variant="standard"
-              fullWidth
-            />
-          </li>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="Warum möchten Sie Mitglied bei uns werden?"
-              variant="outlined"
-              minRows={3}
-              multiline
-              fullWidth
-            />
-          </li>
-        </ul>
-        <p>
-          Möchten sie aktiv bei der Vereinsplanung und Struktur mitwirken? xxxx
-        </p>
-        <FormControl>
-          <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
-          <RadioGroup
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="female"
-            name="radio-buttons-group"
-            row
-          >
-            <FormControlLabel value="Ja" control={<Radio />} label="Ja" />
-            <FormControlLabel
-              value="Vielleicht"
-              control={<Radio />}
-              label="Vielleicht"
-            />
-            <FormControlLabel value="Nein" control={<Radio />} label="Nein" />
-          </RadioGroup>
-        </FormControl>
       </div>
 
+      <h2>Freiwillige angaben </h2>
+      <ul>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="Beruf/Tätigkeitsort"
+            variant="standard"
+            fullWidth
+          />
+        </li>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="Warum möchten Sie Mitglied bei uns werden?"
+            variant="outlined"
+            minRows={3}
+            multiline
+            fullWidth
+          />
+        </li>
+      </ul>
       <div>
-        <h1>SEPA-Lastschriftmandat</h1>
+        <p>Möchten sie aktiv bei der Vereinsplanung und Struktur mitwirken?</p>
+      </div>
+      <FormControl>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+          row
+        >
+          <FormControlLabel value="Ja" control={<Radio />} label="Ja" />
+          <FormControlLabel
+            value="Vielleicht"
+            control={<Radio />}
+            label="Vielleicht"
+          />
+          <FormControlLabel value="Nein" control={<Radio />} label="Nein" />
+        </RadioGroup>
+      </FormControl>
+
+      <h1>SEPA-Lastschriftmandat</h1>
+      <div>
         <p>
-          Gläubiger-Identifikationsnummer: DE27ZZZ00002425162
-          Hier mit ermächtige ich, dass der Erfindergeist Jülich mittels Lastschrift die im Antrag
-          angekreuzte Zahlung einziehen darf. Zugleich weise ich mein
+          Gläubiger-Identifikationsnummer: DE27ZZZ00002425162 Hier mit
+          ermächtige ich, dass der Erfindergeist Jülich mittels Lastschrift die
+          im Antrag angekreuzte Zahlung einziehen darf. Zugleich weise ich mein
           Kreditinstitut an, die vom Erfindergeist Jülich e.V. auf mein Konto
           gezogene Lastschrift einzulösen.
         </p>
-        <ul>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="Kontoinhaber"
-              variant="standard"
-              fullWidth
-            />
-          </li>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="Kreditinstitut"
-              variant="standard"
-              fullWidth
-            />
-          </li>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="IBAN"
-              variant="standard"
-              fullWidth
-            />
-          </li>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="BIC"
-              variant="standard"
-              fullWidth
-            />
-          </li>
-          <li>
-            <TextField
-              id="standard-basic"
-              label="Unterschrift"
-              variant="standard"
-              fullWidth
-              disabled
-            />
-          </li>
-        </ul>
+      </div>
+      <ul>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="Kontoinhaber"
+            variant="standard"
+            fullWidth
+          />
+        </li>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="Kreditinstitut"
+            variant="standard"
+            fullWidth
+          />
+        </li>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="IBAN"
+            variant="standard"
+            fullWidth
+          />
+        </li>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="BIC"
+            variant="standard"
+            fullWidth
+          />
+        </li>
+        <li>
+          <TextField
+            id="standard-basic"
+            label="Unterschrift"
+            variant="standard"
+            fullWidth
+            disabled
+          />
+        </li>
+      </ul>
+      <div>
         <p>
           abschicken an: <br></br>
           Erfindergeist Jülich e.V. <br></br>
@@ -333,6 +328,12 @@ function App() {
           </a>{" "}
           abgeben.
         </p>
+      </div>
+
+      <div className="no-print">
+        <Button variant="contained" onClick={() => window.print()}>
+          Drucken
+        </Button>
       </div>
     </>
   );
---
+++ src/ContributeTable.tsx
@@ -0,0 +1,102 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import RadioWrapper from "./RadioWrapper";
+import React from "react";
+import { TextField } from "@mui/material";
+function createData(key: string, name: string, month: number) {
+  return { key, name, month };
+}
+
+const rows = [
+  createData("a", "- ordentliches Mitglied", 10),
+  createData("b", "- Förder Mitgliedschaft: 1. Natürliche Personen", 10),
+  createData("c", "- Förder Mitgliedschaft: 2. Juristische Personen", 15),
+  createData("d", "- Individuelle Förderung:", 0),
+];
+
+export default function ContributeTable() {
+  const [selectedValue, setSelectedValue] = React.useState<string>("nothing");
+  const handleRadioChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    checked: boolean
+  ) => {
+    if (checked) {
+      setSelectedValue(event.target.value);
+    }
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Mitgliedsbeitragsmodell</TableCell>
+            <TableCell align="right">Monatlich</TableCell>
+            <TableCell align="right">Jährlich</TableCell>
+            <TableCell align="right">Auswahl</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => {
+            if (row.key === "d") {
+              return (
+                <TableRow
+                  key={row.key}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell colSpan={3} component="th" scope="row">
+                    {row.name}
+                    <br />
+                    Ich fördere den Erfindergeist Jülich e.V. mit folgendem
+                    Betrag (in Euro, pro Monat):
+                    <TextField
+                      id="standard-basic"
+                      label="Monats Beitrag"
+                      variant="standard"
+                      fullWidth
+                    />
+                    Dieser Betrag wird gesammelt, einmal jährlich überwiesen
+                    oder abgebucht. Juristische Personen geben bitte den Namen
+                    einer natürlichen Person als Vertreter an
+                    (Unternehmensname).
+                  </TableCell>
+                  <TableCell align="right">
+                    <RadioWrapper
+                      checkValue={row.key}
+                      selectedValue={selectedValue}
+                      onChange={handleRadioChange}
+                    />
+                  </TableCell>
+                </TableRow>
+              );
+            }
+            return (
+              <TableRow
+                key={row.key}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.month}€</TableCell>
+                <TableCell align="right">{row.month * 12}€</TableCell>
+                <TableCell align="right">
+                  <RadioWrapper
+                    checkValue={row.key}
+                    selectedValue={selectedValue}
+                    onChange={handleRadioChange}
+                  />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
