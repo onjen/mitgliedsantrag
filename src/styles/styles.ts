a4f615dd30443980d0ee3a3923c7d8829ad83512
Lars Eschweiler <lars.eschweiler@novacapta.com>
2024-10-08 10:21:09 +0200

    fix: scrollable modals

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -3,11 +3,15 @@ export const modalBoxStyle = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
+  maxHeight: "90vh",
   maxWidth: "90vw",
   width: 800,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
+  overflow: "scroll",
+  overflowX: "hidden",
+  overflowY: "auto"
 
 };
